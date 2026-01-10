b85d037a5807eb75a226c9ae2da7c59daec32b2c
sbtager <tagerar@gmail.com>
2026-01-10 18:20:30 +0100

    Delete bots/bot_1768049407037 directory

--- bots/bot_1768049407037/Sato-block.js
+++
@@ -1,8 +0,0 @@
-// Sato-block.js
-const BOT_NAME='bot_1768049407037';
-const PREFIX='.';
-const DEV_NAME='developer';
-const DEV_ID='0';
-
-// هنا يكتب باقي كود Sato-block.js
-// الأوامر والإجابات وكل شيء ثابت
